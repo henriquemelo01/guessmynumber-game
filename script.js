f4222c5caa5250ce3f898ca32e287c8e72a6c12f
Henrique Moraes <henriquemsm@outlook.com>
2020-12-19 11:22:41 -0300

    Implementing check button

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// Implementing Check button:
+const btnCheck = document.querySelector(".check");
+btnCheck.addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value); // We need to convert the type of the input, because it's a string
+  console.log(guess, typeof guess); // Testing if the check button function is working
+});
